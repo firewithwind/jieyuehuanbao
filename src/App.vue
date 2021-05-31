beccfe708b4ed34a90dcd14e7f7b921647a2b711
liulianxing <liulianxing@meituan.com>
2021-05-31 10:38:37 +0800

    初始化配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <div id="nav">
+      <router-link to="/">首页</router-link>
+      <router-link to="/about">关于我们</router-link>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+html {
+  height: 100%;
+}
+body {
+  margin: 0;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  line-height: 50px;
+  background-color: #70d486;
+  flex: 1;
+}
+
+#nav a.router-link-exact-active {
+  color: #fff;
 }
 </style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="title">山东捷越环保有限公司</div>
+    <div class="category">
+      <div v-for="item in categoryList" :key="item.title" class="category-item">
+        <!-- <van-icon :name="item.icon" /> -->
+        <i :class="item.icon"></i>
+        <span>{{item.title}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+      return {
+        categoryList: [
+          {
+            title: '产品展示',
+            icon: 'icon-app',
+            route: ''
+          },
+          {
+            title: '新闻资讯',
+            icon: '',
+            route: ''
+          },
+          {
+            title: '联系我们',
+            icon: '',
+            route: ''
+          }
+        ]
+      }
+    },
+    methods: {}
+}
+</script>
+<style>
+.title {
+  line-height: 50px;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0 10px;
+  border-bottom: 1px solid #2c3e50;
+}
+.category {
+  display: flex;
+  justify-content: space-around;
+  height: 30px;
+  font-size: 14px;
+}
+
+.category-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.van-icon {
+  height: 30px;
+  width: 30px;
+  font-size: 30px;
+  line-height: 30px !important;
+}
+</style>
